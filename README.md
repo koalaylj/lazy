05d71206d09d694210fea321efb1b6b412f80d32
yulijun <koalaylj@126.com>
2016-03-16 11:18:06 +0900

    add atom & conemu config files

---
+++ README.md
@@ -0,0 +1,8 @@
+配置文件和脚本
+============
+
+
+### 说明
+* config 目录
+	* `atom-package.sh` atom编辑器常用插件
+	* `ConEmu.xml` ConEmu 配置文件
---
+++ config/atom-package.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# my favoriate atom packages 
+
+apm install atom-beautify
+apm install atom-html-preview
+apm install atom-minify
+apm install atom-runner
+apm install autocomplete-modules
+apm install color-picker
+apm install docblockr
+apm install emmet
+apm install highlight-selected
+apm install javascript-snippets
+apm install linter
+apm install linter-jshint
+apm install minimap
+apm install minimap-find-and-replace
+apm install minimap-highlight-selected
+apm install minimap-selection
+apm install node-debugger
+apm install pigments
+apm install pretty-json
+apm install Sublime-Style-Column-Selection
+apm install terminal-plus
+apm install todo-show
